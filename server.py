c903530c480580f8820479c419de89b6ac6783db
Jenya Himmelreich <jenya.himmelreich@sap.com>
2025-01-02 14:46:44 +0200

    web version

--- server.py
+++ server.py
@@ -1,47 +1,22 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-from texttable import Texttable
+from flask import Flask, request, render_template
+import spotify_api
 
-import conf
-import utils
+app = Flask(__name__)
 
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=conf.CLIENT_ID,
-                                                           client_secret=conf.CLIENT_SECRET))
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    tracks = None
+    playlist_name = None
+    playlist_url = None
+    if request.method == 'POST':
+        playlist_url = request.form['playlist_url']
+        # Use the mock_process_playlist function for now
+        playlist_name, tracks = spotify_api.process_playlist(playlist_url)
+    return render_template('index.html', 
+                           playlist_name=playlist_name, 
+                           playlist_url=playlist_url,
+                           tracks=tracks)
 
-results = sp.playlist_items('1OqSCV8pzfAZ6zsAjEsYOM')
-
-tbl = Texttable(max_width=1024)
-tbl.header(['Time','Duration','Title', 'Artist'])
-tbl.set_deco(Texttable.VLINES | Texttable.HEADER | Texttable.BORDER)
-row = []
-
-ttl_duration_ms = 0.1
-for idx, track in enumerate(results['items']):
-    t = track['track']
-    
-    names = [artist['name'] for artist in t['artists']]
-    artists = ', '.join(names)
-
-
-    album = t['album']['name']
-    name = t['name']
-    pop = t['popularity']
-    
-    t_duration_ms = t['duration_ms']
-
-    # id = t['id']
-
-    # analysis = sp.audio_analysis(id)
-
-    #print (f"{utils.milliseconds_to_human_readable(ttl_duration_ms)} | {name} | {album}")
-
-    tbl.add_row([utils.milliseconds_to_human_readable(ttl_duration_ms),
-                 utils.milliseconds_to_human_readable(t_duration_ms),
-                 name,
-                 artists])
-
-    ttl_duration_ms = t_duration_ms + ttl_duration_ms
-
-
-print (tbl.draw())
+if __name__ == '__main__':
+    app.run(debug=True)
