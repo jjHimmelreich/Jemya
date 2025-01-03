23daebfaf81c1fcbe0b5b1c45a707a2a2f7c4383
Jenya Himmelreich <jenya.himmelreich@sap.com>
2025-01-03 18:41:21 +0200

    oauth + favicon

--- utils.py
+++ utils.py
@@ -1,27 +1,8 @@
 import datetime
 
 def milliseconds_to_human_readable(ms):
-    delta = datetime.timedelta(milliseconds=ms)
-    days = delta.days
-    hours, remainder = divmod(delta.seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-
-    human_readable = []
-
-    if days > 0:
-        human_readable.append(f"{days}:")
-
-    if hours > 0:
-        human_readable.append(f'{str(hours).zfill(2)}:')
-    else:
-        human_readable.append('   ')
-
-    if minutes > 0:
-        human_readable.append(f'{str(minutes).zfill(2)}:')
-    else:
-        human_readable.append('00:')
-    
-    human_readable.append(f'{str(seconds).zfill(2)}')
-
-    return ''.join(human_readable)
+    seconds = (ms / 1000) % 60
+    minutes = (ms / (1000 * 60)) % 60
+    hours = (ms / (1000 * 60 * 60)) % 24
+    return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
 
