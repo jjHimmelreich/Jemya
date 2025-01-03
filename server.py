0a8bbb3ebe6493822342a160df72daa12a54873d
Jenya Himmelreich <jenya.himmelreich@sap.com>
2025-01-03 18:41:21 +0200

    oauth + favicon

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, redirect, session, url_for, render_template, jsonify, make_response, send_from_directory
 import spotify_api  # Import your Spotify module
+import os
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Needed for session management
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -10,24 +17,51 @@ def index():
     playlist_url = None
     if request.method == 'POST':
         playlist_url = request.form['playlist_url']
+        session['playlist_url'] = playlist_url
         # Call the actual process_playlist function from spotify_api
+        playlist_name, tracks = spotify_api.process_playlist(playlist_url)
+    elif 'playlist_url' in session:
+        playlist_url = session['playlist_url']
         playlist_name, tracks = spotify_api.process_playlist(playlist_url)
     return render_template('index.html', playlist_name=playlist_name, tracks=tracks, playlist_url=playlist_url)
 
+@app.route('/login')
+def login():
+    # sp_oauth = spotify_api.get_spotify_oauth()
+    # auth_url = sp_oauth.get_authorize_url()
+    # return redirect(auth_url)
+    sp_oauth = spotify_api.get_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    response = make_response(redirect(auth_url))
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    return response
+
+@app.route('/callback')
+def callback():
+    sp_oauth = spotify_api.get_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session['token_info'] = token_info
+    return redirect(url_for('index'))
+
 @app.route('/play', methods=['POST'])
 def play():
-    data = request.get_json()
-    track_id = data['track_id']
-    # Call the function to play the track using the track_id
-    spotify_api.play_track(track_id)
+    token_info = session.get('token_info', None)
+    if not token_info:
+        return redirect(url_for('login'))
+    track_id = request.json['track_id']
+    spotify_api.play_track(track_id, token_info)
     return jsonify(success=True)
 
 @app.route('/pause', methods=['POST'])
 def pause():
-    data = request.get_json()
-    track_id = data['track_id']
-    # Call the function to pause the track using the track_id
-    spotify_api.pause_track(track_id)
+    token_info = session.get('token_info', None)
+    if not token_info:
+        return redirect(url_for('login'))
+    track_id = request.json['track_id']
+    spotify_api.pause_track(track_id, token_info)
     return jsonify(success=True)
 
 if __name__ == '__main__':
--- spotify_api.py
+++ spotify_api.py
@@ -1,94 +1,73 @@
+import utils
+import conf
 import re
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 from texttable import Texttable
-
 pattern = r'playlist/([a-zA-Z0-9]+)'
 
-import conf
-import utils
 
-def init():
-    #spotify_client = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(client_id=conf.CLIENT_ID, client_secret=conf.CLIENT_SECRET))
-    spotify_client = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=conf.CLIENT_ID, client_secret=conf.CLIENT_SECRET))
+def get_spotify_oauth():
+    sp = SpotifyOAuth(client_id=conf.CLIENT_ID, client_secret=conf.CLIENT_SECRET, redirect_uri=conf.REDIRECT_URI, scope="user-modify-playback-state user-read-playback-state")
+    return sp
 
-    return spotify_client
 
-#https://open.spotify.com/playlist/5gmv8rah3E1YMMT1tGgUW0?si=99f17988b5df4353
+def init(token_info=None):
+    if token_info:
+        sp = spotipy.Spotify(auth=token_info['access_token'])
+    else:
+        sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+            client_id=conf.CLIENT_ID, client_secret=conf.CLIENT_SECRET))
+    return sp
+
+
 def process_playlist(playlist_link):
-
     track_objects = []
-
-    #playlist_link = input(f"Paste link to playlist here:")
-    
-    # Search for the pattern in the input string
     match = re.search(pattern, playlist_link)
     playlist_id = None
-
-    # Extract the playlist ID
     if match:
         playlist_id = match.group(1)
-
-    #sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=conf.CLIENT_ID, client_secret=conf.CLIENT_SECRET))
-    sp = init()
-
+    sp = init()  # No token_info needed for fetching playlist details
     playlist = sp.playlist(playlist_id)
-
     playlist_name = playlist['name']
-
-
-    #results = sp.playlist_items(playlist_id)
     tracks = playlist['tracks']['items']
-
-    tbl = Texttable(max_width=1024)
-    tbl.header(['Time','Duration','Title', 'Artist'])
-    tbl.set_deco(Texttable.VLINES | Texttable.HEADER | Texttable.BORDER)
-    row = []
-
     ttl_duration_ms = 0.1
     for idx, track in enumerate(tracks):
         t = track['track']
-        
         names = [artist['name'] for artist in t['artists']]
         artists = ', '.join(names)
-
         album = t['album']['name']
         name = t['name']
         pop = t['popularity']
-
         id = t['id']
         track_url = t['external_urls']['spotify']
 
-        album_image = t['album']['images'][0]['url']  #640, 300, 64
-
+        album_image = t['album']['images'][0]['url']
         t_duration_ms = t['duration_ms']
-
         ttl_duration_ms = t_duration_ms + ttl_duration_ms
-
         track_objects.append(
             {
-                "idx":idx,
+                "idx": idx,
                 "id": id,
-                "url":track_url,
+                "url": track_url,
                 "name": name,
                 "album": album,
                 "album_image_url": album_image,
                 "author": artists,
                 "duration": utils.milliseconds_to_human_readable(t_duration_ms),
-                "duration_from_start": utils.milliseconds_to_human_readable(ttl_duration_ms-t_duration_ms)
+                "duration_from_start": utils.milliseconds_to_human_readable(ttl_duration_ms - t_duration_ms)
             }
         )
+    return playlist_name, track_objects
 
-    return playlist_name,track_objects
 
-def play_track(track_id):
-    # sp = init()
-    # sp.start_playback()
-    print('play track')
-    return
+def play_track(track_id, token_info):
+    sp = init(token_info)
+    sp.start_playback(uris=[f'spotify:track:{track_id}'])
+    print(f'Playing track: {track_id}')
 
-def pause_track(track_id):
-    # sp = init()
-    # sp.pause_playback()
-    print('pause track')
-    return
+
+def pause_track(track_id, token_info):
+    sp = init(token_info)
+    sp.pause_playback()
+    print(f'Pausing track: {track_id}')
