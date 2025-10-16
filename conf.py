2bf13af84bbe438f167d6c190635f3e7f30c44fb
Jenya Himmelreich <jenya.himmelreich@sap.com>
2025-10-16 12:12:30 +0300

    🔒 Remove conf.py from Git tracking and add template
    - Remove conf.py from version control (contains sensitive data)
    - Add conf.py.template for developers to copy and configure
    - conf.py is now properly ignored by .gitignore

--- conf.py
+++
@@ -1,8 +0,0 @@
-import os
-
-CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
-REDIRECT_URI = 'http://localhost:5555/callback'
-
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-
